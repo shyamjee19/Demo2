c2889d7b25263f3eebeabc1c694d308ec6b23c0d
shyamjee19 <shyamjees538@gmail.com>
2025-07-03 17:24:52 +0530

    4

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { act } from 'react';
 import '@testing-library/jest-dom'; // Add this line
-import App from './App';
+import App from './app';
 
 test('renders welcome message', () => {
   act(() => {
